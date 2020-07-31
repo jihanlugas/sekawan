6a40685e24b6882c4b9ea0f14bc7c01fdfe4b94d
Jihan Lugas <jihanlugas2@gmail.com>
2020-07-31 21:36:56 +0700

    tes

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -13,7 +13,7 @@
                         <div class="px-6 py-4">
                             <div class="font-bold text-xl mb-2"> {{ $i }} The Coldest Sunset</div>
                             <p class="text-gray-700 text-base">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                                tes ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                             </p>
                         </div>
                         <div class="px-6 py-4">
